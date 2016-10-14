f361f7a69a4f21b97232aaa05782791cc6b7e153
wanheming <diwers@163.com>
2016-10-14 11:28:38 +0800

    优化

--- start.sh
+++ start.sh
@@ -1,1 +1,26 @@
-docker run -it -h hadoop --name base -p 10022:22  mshengs/hadoop:base  
+sed -i “” '/^[localhost]:10022/d' /Users/ming/.ssh/known_hosts
+docker rm master -f
+docker network create --driver=bridge hadoop
+#docker run -it -h hadoop --name master -p 10022:22  mshengs/hadoop:base  
+N=${1:-3}
+docker run -itd \
+                --net=hadoop \
+                -p 50070:50070 \
+                -p 8088:8088 \
+                -p 10022:22 \
+                --name hadoop-master \
+                --hostname hadoop-master \
+                mshengs/hadoop:base 
+i=1
+while [ $i -lt $N ]
+do
+	sudo docker rm -f hadoop-slave$i &> /dev/null
+	echo "start hadoop-slave$i container..."
+	sudo docker run -itd \
+	                --net=hadoop \
+                    -p 10022:22\
+	                --name hadoop-slave$i \
+	                --hostname hadoop-slave$i \
+	                mshengs/hadoop:base /bin/bash
+	i=$(( $i + 1 ))
+done 
