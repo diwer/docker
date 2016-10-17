05406a74cb6404b473340bc868b853743ab46b22
wanheming <diwers@163.com>
2016-10-17 10:06:24 +0800

    hadoop dockerfile 上传

--- hadoop/build.sh
+++ hadoop/build.sh
@@ -1,3 +1,6 @@
 source ../clear.sh
 docker rmi mshengs/hadoop:base -f
+
 docker build -t mshengs/hadoop:base .
+docker login --username=mshengs --password=diwers@163
+docker push mshengs/hadoop:base
