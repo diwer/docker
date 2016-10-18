5b647d751eac648cf8989e316bb2501735f97a35
wanheming <diwers@163.com>
2016-10-18 11:44:13 +0800

    fix

--- hadoop/build.sh
+++ hadoop/build.sh
@@ -8,4 +8,4 @@ docker rmi mshengs/hadoop:base -f
 
 docker build -t mshengs/hadoop:base .
 docker login --username=mshengs --password=diwers@163
-#docker push mshengs/hadoop:base
+docker push mshengs/hadoop:base
